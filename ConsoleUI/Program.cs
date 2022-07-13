f613de42aad1fc61e54246135f870f366a0b40e5
SENANUR KOTANKIRAN <senanurkotankiran@posta.mu.edu.tr>
2022-07-13 22:20:59 +0300

    Core katmani olusturuldu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,26 +10,51 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+
+            //BrandTest();
+
             CarManager carManager = new CarManager(new EfCarDal());
-            //Car car1 = new Car();
-            //car1.BrandId = 1;
-            //car1.CarName = "Fiat Doblo";
-            //car1.ColorId = 1;
-            //car1.DailyPrice = 475;
-            //car1.Description = "Manuel Vites";
-            //car1.ModelYear = 2016;
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.CarName + "  " + car.BrandName + " " + car.ColorName + " " + car.DailyPrice);
+            }
+           
 
-            //carManager.Add(car1);
+        }
+
+        private static void BrandTest()
+        {
+            Brand brand1 = new Brand();
+            brand1.BrandName = "Mercedes";
+
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            brandManager.Add(brand1);
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            Car car1 = new Car();
+            car1.BrandId = 1;
+            car1.CarName = "Fiat Doblo";
+            car1.ColorId = 1;
+            car1.DailyPrice = 475;
+            car1.Description = "Manuel Vites";
+            car1.ModelYear = 2016;
+
+            carManager.Add(car1);
 
 
 
-            foreach (var car in carManager.GetCarsByBrandId(1))
+            foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.CarName);
             }
-
-           
-
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,37 +1,13 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal : IBrandDal
+    public class EfBrandDal : EfEntityRepositoryBase<Brand, ReCapProjectDbContext>, IBrandDal
     {
-        public void Add(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Brand Get(Expression<Func<Brand, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
