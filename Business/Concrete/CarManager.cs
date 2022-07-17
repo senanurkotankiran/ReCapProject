027ba785daaa552a19b75f887ec4fe22f9b028f6
SENANUR KOTANKIRAN <senanurkotankiran@posta.mu.edu.tr>
2022-07-17 20:10:53 +0300

    Autofac ,FluentValidation destegi eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,17 +22,14 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.CarName.Length >= 2 && car.DailyPrice > 0)
-            {
-                    _carDal.Add(car);
+
+                
+                _carDal.Add(car);
                 return new Result(true,Messages.CarAdded);
-            }
-            else
-            {
-                return new ErrorResult( Messages.CarNameInValid);
-            }
+           
         }
 
         public IResult Delete(Car car)
--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,14 +21,13 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
         }
 
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
-            if (rental.ReturnDate == null) 
-            {
+          
                 _rentalDal.Add(rental);
                 return new SuccessResult(Messages.RentalAdded);
-            }
-            return new ErrorResult(Messages.RentalDateIsNull);
+        
             
            
             
